9df21a4946f381c854f3b7333b46aa22141a8fc6
Nishide Daisuke <d.nishide0125@gmail.com>
2014-11-26 06:36:26 +0000

    フォーム送信用HTMLひな形ファイルまで作成

---
+++ online_bbs/bbs.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>ひとこと掲示板</title>
+</head>
+<body>
+    <h1>ひとこと掲示板</h1>
+
+    <form action="bbs.php" method="POST">
+        名前: <input type="text" name="name" /><br />
+        ひとこと: <input type="text" name="comment" size='60' /><br />
+        <input type="submit" name="submit" value="送信" />
+    </form>
+</body>
+</html>
