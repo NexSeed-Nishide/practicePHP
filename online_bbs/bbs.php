936130851dda5802418cd3b2210c391f09dfcd97
Nishide Daisuke <d.nishide0125@gmail.com>
2014-11-27 05:40:15 +0000

    保存されたコメント内容を取得して表示する部分を作成

--- online_bbs/bbs.php
+++ online_bbs/bbs.php
@@ -71,5 +71,28 @@
         ひとこと: <input type="text" name="comment" size='60' /><br />
         <input type="submit" name="submit" value="送信" />
     </form>
+
+    <?php
+        //投稿された内容を取得するSQLを作成して結果を取得
+        $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
+        $result = mysql_query($sql, $link);
+    ?>
+    <?php if($result !== false && mysql_num_rows($result)): ?>
+        <ul>
+            <?php while($post = mysql_fetch_assoc($result)): ?>
+            <li>
+                <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>:
+                <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
+                - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
+            </li>
+            <?php endwhile; ?>
+        </ul>
+    <?php endif; ?>
+
+    <?php
+        //取得結果を解放して接続を閉じる
+        mysql_free_result($result);
+        mysql_close($link);
+    ?>
 </body>
 </html>
