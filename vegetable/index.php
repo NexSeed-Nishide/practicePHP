9594bedbbfe2edce510ca93c0780a5e4f2db0b8c
Nishide Daisuke <d.nishide0125@gmail.com>
2014-12-04 06:14:54 +0000

    とりあえずサンプルをコミット

--- vegetable/index.php
+++ vegetable/index.php
@@ -1,3 +1,100 @@
+<?php
+
+    /**
+     * データベースとコネクションを作成
+     */
+    function connectDatabase() {
+        $link = mysql_connect("localhost", "nishide", "daisuke");
+        mysql_query('SET NAMES utf8', $link);
+        mysql_select_db("vegetable", $link);
+
+        return $link;
+    }
+
+    /**
+     * カテゴリのデータを全て取得
+     */
+    function getCategories() {
+        $link = connectDatabase();
+
+        $query = "SELECT * FROM categories";
+        $result = mysql_query($query, $link);
+
+        $rows = mysql_num_rows($result);
+        $categories = array();
+        for ($i = 0; $i < $rows; $i++) {
+            $categories[] = mysql_fetch_assoc($result);
+        }
+
+        return $categories;
+    }
+
+    /**
+     * itemテーブルからのデータを全て取得
+     */
+    function getItems() {
+        $link = connectDatabase();
+
+        $query = "SELECT * FROM items";
+        $result = mysql_query($query, $link);
+
+        $rows = mysql_num_rows($result);
+        $items = array();
+        for ($i = 0; $i < $numRows; $i++) {
+            $items[] = mysql_fetch_assoc($result);
+        }
+
+        return $items;
+    }
+
+    /**
+     * 全てのデータを取得する
+     */
+    function getAllItems() {
+        $link = connectDatabase();
+
+        $query = "SELECT * FROM categories, items WHERE categories.id = items.category_id";
+        $result = mysql_query($query, $link);
+
+        $rows = mysql_num_rows($result);
+        $datas = array();
+        for ($i = 0; $i < $rows; $i++) {
+            $datas[] = mysql_fetch_assoc($result);
+        }
+
+        return $datas;
+    }
+
+    /**
+     * 特定のカテゴリのアイテム一覧を取得する
+     */
+    function getItemsWithSpecificCatetory($categoryId) {
+        $link = connectDatabase();
+        $query = "SELECT
+                    categories.id AS category_id,
+                    items.id AS item_id,
+                    categories.name AS category_name,
+                    items.name AS item_name
+                FROM
+                    categories, items
+                WHERE
+                    categories.id = items.category_id AND
+                    categories.id = $categoryId";
+        $result = mysql_query($query, $link);
+
+        $rows = mysql_num_rows($result);
+        $datas = array();
+        for ($i = 0; $i < $rows; $i++) {
+            $datas[] = mysql_fetch_assoc($result);
+        }
+
+        return $datas;
+    }
+
+    $categories = getCategories();
+    $items = getItems();
+
+?>
 <html>
 <head>
     <meta charset="UTF-8" />
@@ -6,20 +103,24 @@
 <body>
     <h1>野菜一覧だよ</h1>
     <div>
-        <div>
-            <p>Category1</p>
-            <ul>
-                <li>item 1</li>
-                <li>item 2</li>
-            </ul>
-        </div>
-        <div>
-            <p>Category2</p>
-            <ul>
-                <li>item 1</li>
-                <li>item 2</li>
-            </ul>
-        </div>
+        <ul>
+        <?php
+            foreach ($categories as $category) {
+                $items = getItemsWithSpecificCatetory($category['id']);
+
+                echo "<div>";
+                echo "<h2>".$category['name']."</h2>";
+                echo "<ul>";
+                foreach ($items as $item) {
+                    echo "<li>";
+                    echo $item['item_name'];
+                    echo "</li>";
+                }
+                echo "</ul>";
+                echo "</div>";
+            }
+        ?>
+        </ul>
     </div>
 </body>
 </html>
